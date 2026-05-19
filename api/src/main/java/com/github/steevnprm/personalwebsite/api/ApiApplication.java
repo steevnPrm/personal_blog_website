d553be34cafc31164fbe9a8415ec6b24ba639f47
steevnprm <steevn.pyram@gmail.com>
2026-05-19 11:56:03 +0200

    docs(readme): align configuration model and clarify environment-based JWT secret handling

--- api/src/main/java/com/github/steevnprm/personalwebsite/api/ApiApplication.java
+++ api/src/main/java/com/github/steevnprm/personalwebsite/api/ApiApplication.java
@@ -1,18 +1,32 @@
 package com.github.steevnprm.personalwebsite.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication(
-	exclude = {
-		DataSourceAutoConfiguration.class
-	}
+    exclude = {
+        DataSourceAutoConfiguration.class
+    }
 )
 public class ApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
 
+@Component
+class JwtDebugPrinter implements CommandLineRunner {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Override
+    public void run(String... args) {
+        System.out.println("JWT SECRET = " + jwtSecret);
+    }
 }
