0eafe41ed0b77a340ce12a4910ee206881700c2e
steevnprm <steevn.pyram@gmail.com>
2026-05-25 21:48:54 +0200

    chore: configure server port and database connection variables

--- api/src/main/java/com/github/steevnprm/personalwebsite/api/ApiApplication.java
+++ api/src/main/java/com/github/steevnprm/personalwebsite/api/ApiApplication.java
@@ -2,19 +2,11 @@ package com.github.steevnprm.personalwebsite.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ApiApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(ApiApplication.class, args);
-        
-        // Diagnostic : affiche la valeur réelle vue par Spring
-        Environment env = context.getEnvironment();
-        System.out.println("--- DIAGNOSTIC DES VARIABLES ---");
-        System.out.println("DB_URL détecté : " + env.getProperty("DB_URL"));
-        System.out.println("DB_USERNAME détecté : " + env.getProperty("DB_USERNAME"));
+        SpringApplication.run(ApiApplication.class, args);
     }
 }
