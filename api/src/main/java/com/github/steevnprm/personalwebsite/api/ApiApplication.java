2671284cbee53e2e15a5d9f843c907445f2a7795
steevnprm <steevn.pyram@gmail.com>
2026-05-25 19:35:47 +0200

    ci: automatiser la construction et la publication de l'image Docker sur GHCR

--- api/src/main/java/com/github/steevnprm/personalwebsite/api/ApiApplication.java
+++ api/src/main/java/com/github/steevnprm/personalwebsite/api/ApiApplication.java
@@ -1,27 +1,12 @@
 package com.github.steevnprm.personalwebsite.api;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class ApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
-    }
-}
-
-@Component
-class JwtDebugPrinter implements CommandLineRunner {
-
-    @Value("${jwt.secret}")
-    private String jwtSecret;
-
-    @Override
-    public void run(String... args) {
-        System.out.println("JWT SECRET = " + jwtSecret);
     }
 }
